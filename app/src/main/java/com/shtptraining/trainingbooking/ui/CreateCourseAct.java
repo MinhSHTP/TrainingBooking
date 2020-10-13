a4817d98540f186a990ddd28456daa4dd982dda0
Huang Ming <Huang Ming@DESKTOP-N77ELPL>
2020-10-14 00:26:19 +0700

    DONE Mapping controls in Creat Course Act - 14/10/2020.

--- app/src/main/java/com/shtptraining/trainingbooking/ui/CreateCourseAct.java
+++ app/src/main/java/com/shtptraining/trainingbooking/ui/CreateCourseAct.java
@@ -2,6 +2,8 @@ package com.shtptraining.trainingbooking.ui;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,12 +14,32 @@ import com.shtptraining.trainingbooking.R;
 public class CreateCourseAct extends AppCompatActivity {
     private String TAG = "CreateCourseAct";
 
+    public EditText _et_name_course, _et_duration_date_course,
+            _et_duration_course, _et_duration_time_course,
+            _et_fee_course, _et_numberOf_course;
+
+    public Spinner _spinner_course_trainer;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_course);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        _et_name_course = (EditText) findViewById(R.id.et_name_course);
+        _et_duration_date_course = (EditText) findViewById(R.id.et_duration_date_course);
+        _et_duration_course = (EditText) findViewById(R.id.et_duration_course);
+        _et_duration_time_course = (EditText) findViewById(R.id.et_duration_time_course);
+        _et_fee_course = (EditText) findViewById(R.id.et_fee_course);
+        _et_numberOf_course = (EditText) findViewById(R.id.et_numberOf_course);
+
+        _spinner_course_trainer = (Spinner) findViewById(R.id.spinner_course_trainer);
     }
 
     @Override
