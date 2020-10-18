a77ed4d9420a8a674ec4f86bf6ef49438b7d8feb
Huang Ming <Huang Ming@DESKTOP-N77ELPL>
2020-10-19 00:28:44 +0700

    WORKING event controls in Creat Course Act - 18/10/2020.

--- app/src/main/java/com/shtptraining/trainingbooking/ui/CreateCourseAct.java
+++ app/src/main/java/com/shtptraining/trainingbooking/ui/CreateCourseAct.java
@@ -2,7 +2,10 @@ package com.shtptraining.trainingbooking.ui;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
@@ -11,7 +14,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.shtptraining.trainingbooking.R;
 
-public class CreateCourseAct extends AppCompatActivity {
+public class CreateCourseAct extends AppCompatActivity implements View.OnClickListener {
     private String TAG = "CreateCourseAct";
 
     public EditText _et_name_course, _et_duration_date_course,
@@ -19,6 +22,10 @@ public class CreateCourseAct extends AppCompatActivity {
             _et_fee_course, _et_numberOf_course;
 
     public Spinner _spinner_course_trainer;
+
+    public Button _btn_time_course, _btn_date_course;
+
+    public ImageView _datepicker_start_date_course, _iv_status_course;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -40,6 +47,12 @@ public class CreateCourseAct extends AppCompatActivity {
         _et_numberOf_course = (EditText) findViewById(R.id.et_numberOf_course);
 
         _spinner_course_trainer = (Spinner) findViewById(R.id.spinner_course_trainer);
+
+        _btn_time_course = (Button) findViewById(R.id.btn_time_course);
+        _btn_date_course = (Button) findViewById(R.id.btn_date_course);
+
+        _datepicker_start_date_course = (ImageView) findViewById(R.id.datepicker_start_date_course);
+        _iv_status_course = (ImageView) findViewById(R.id.iv_status_course);
     }
 
     @Override
@@ -55,5 +68,19 @@ public class CreateCourseAct extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         super.onBackPressed();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_time_course:
+                break;
+            case R.id.btn_date_course:
+                break;
+            case R.id.iv_status_course:
+                break;
+            case R.id.datepicker_start_date_course:
+                break;
+        }
     }
 }
