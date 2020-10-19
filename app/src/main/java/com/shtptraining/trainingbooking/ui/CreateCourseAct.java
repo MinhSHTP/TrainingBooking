03e6de572167b8c74792b6458d6f755373d45faf
Huang Ming <Huang Ming@DESKTOP-N77ELPL>
2020-10-20 00:51:58 +0700

    WORKING event controls in Creat Course Act - 20/10/2020.

--- app/src/main/java/com/shtptraining/trainingbooking/ui/CreateCourseAct.java
+++ app/src/main/java/com/shtptraining/trainingbooking/ui/CreateCourseAct.java
@@ -1,18 +1,24 @@
 package com.shtptraining.trainingbooking.ui;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TimePicker;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.shtptraining.trainingbooking.R;
+
+import java.util.Calendar;
 
 public class CreateCourseAct extends AppCompatActivity implements View.OnClickListener {
     private String TAG = "CreateCourseAct";
@@ -26,6 +32,17 @@ public class CreateCourseAct extends AppCompatActivity implements View.OnClickLi
     public Button _btn_time_course, _btn_date_course;
 
     public ImageView _datepicker_start_date_course, _iv_status_course;
+
+    public TimePickerDialog _timePickerDialog;
+
+    public DatePickerDialog _datePickerDialog;
+
+    private int _lastSelectedYear;
+    private int _lastSelectedMonth;
+    private int _lastSelectedDayOfMonth;
+
+    private int _lastSelectedHour;
+    private int _lastSelectedMinutes;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -53,6 +70,12 @@ public class CreateCourseAct extends AppCompatActivity implements View.OnClickLi
 
         _datepicker_start_date_course = (ImageView) findViewById(R.id.datepicker_start_date_course);
         _iv_status_course = (ImageView) findViewById(R.id.iv_status_course);
+
+        _datepicker_start_date_course.setOnClickListener(this);
+        _iv_status_course.setOnClickListener(this);
+        _btn_time_course.setOnClickListener(this);
+        _btn_date_course.setOnClickListener(this);
+
     }
 
     @Override
@@ -72,10 +95,51 @@ public class CreateCourseAct extends AppCompatActivity implements View.OnClickLi
 
     @Override
     public void onClick(View v) {
+        final Calendar cldr = Calendar.getInstance();
         switch (v.getId()) {
             case R.id.btn_time_course:
+                _lastSelectedHour = cldr.get(Calendar.HOUR_OF_DAY);
+                _lastSelectedMinutes = cldr.get(Calendar.MINUTE);
+                _timePickerDialog = new TimePickerDialog(CreateCourseAct.this, android.R.style.Theme_Holo_Light_Dialog_NoActionBar, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        _btn_time_course.setText(hourOfDay + ":" + minute);
+                        _lastSelectedHour = hourOfDay;
+                        _lastSelectedMinutes = minute;
+                    }
+                }, _lastSelectedHour, _lastSelectedMinutes, true);
+                _timePickerDialog.show();
+
                 break;
             case R.id.btn_date_course:
+                _lastSelectedDayOfMonth = cldr.get(Calendar.DAY_OF_MONTH);
+                _lastSelectedMonth = cldr.get(Calendar.MONTH);
+                _lastSelectedYear = cldr.get(Calendar.YEAR);
+                // Date Select Listener.
+                DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+
+                        String dayString = String.valueOf(dayOfMonth).length() == 1 ? "0" + dayOfMonth : String.valueOf(dayOfMonth);
+
+                        String monthString = String.valueOf(monthOfYear).length() == 1 ? "0" + (monthOfYear + 1) : String.valueOf(monthOfYear + 1);
+
+                        _btn_date_course.setText(dayString + "-" + monthString + "-" + year);
+
+                        _lastSelectedYear = year;
+                        _lastSelectedMonth = monthOfYear;
+                        _lastSelectedDayOfMonth = dayOfMonth;
+                    }
+                };
+
+                _datePickerDialog = new DatePickerDialog(this,
+                        android.R.style.Theme_Holo_Light_Dialog_NoActionBar,
+                        dateSetListener, _lastSelectedYear, _lastSelectedMonth, _lastSelectedDayOfMonth);
+
+                // Show
+                _datePickerDialog.show();
                 break;
             case R.id.iv_status_course:
                 break;
