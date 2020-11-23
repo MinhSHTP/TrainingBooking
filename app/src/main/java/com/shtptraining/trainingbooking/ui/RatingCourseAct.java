fbcacba72210cae19c2227132a3cf2dd102558da
Huang Ming <Huang Ming@DESKTOP-N77ELPL>
2020-11-24 02:36:11 +0700

    Done add smile rating in Rating Course Activity - 24/11/2020.

--- app/src/main/java/com/shtptraining/trainingbooking/ui/RatingCourseAct.java
+++ app/src/main/java/com/shtptraining/trainingbooking/ui/RatingCourseAct.java
@@ -7,13 +7,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RatingBar;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.hsalf.smilerating.BaseRating;
+import com.hsalf.smilerating.SmileRating;
 import com.shtptraining.trainingbooking.Commons.CallAPIs.CallWebAPI;
 import com.shtptraining.trainingbooking.Commons.Constants;
 import com.shtptraining.trainingbooking.Commons.Helpers;
@@ -34,10 +35,14 @@ public class RatingCourseAct extends AppCompatActivity {
 
     public static CallWebAPI _callWebAPI = retrofit.create(CallWebAPI.class);
 
-    public RatingBar _rb_question_1, _rb_question_2, _rb_question_3, _rb_question_4,
+//    public RatingBar _rb_question_1, _rb_question_2, _rb_question_3, _rb_question_4,
+//            _rb_question_5, _rb_question_6, _rb_question_7, _rb_question_8, _rb_question_9,
+//            _rb_question_10;
+
+    public SmileRating _rb_question_1, _rb_question_2, _rb_question_3, _rb_question_4,
             _rb_question_5, _rb_question_6, _rb_question_7, _rb_question_8, _rb_question_9,
             _rb_question_10;
-
+    //    public SmileRating _smile_rating_question_1;
     public EditText _et_question_11, _et_question_12, _et_question_13,
             _et_question_14, _et_question_15;
 
@@ -61,33 +66,43 @@ public class RatingCourseAct extends AppCompatActivity {
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         _rb_question_1 = findViewById(R.id.rb_question_1);
+        InitializeSmileRating(_rb_question_1);
         _rb_question_1.setTag(1);
 
         _rb_question_2 = findViewById(R.id.rb_question_2);
+        InitializeSmileRating(_rb_question_2);
         _rb_question_2.setTag(2);
 
         _rb_question_3 = findViewById(R.id.rb_question_3);
+        InitializeSmileRating(_rb_question_3);
         _rb_question_3.setTag(3);
 
         _rb_question_4 = findViewById(R.id.rb_question_4);
+        InitializeSmileRating(_rb_question_4);
         _rb_question_4.setTag(4);
 
         _rb_question_5 = findViewById(R.id.rb_question_5);
+        InitializeSmileRating(_rb_question_5);
         _rb_question_5.setTag(5);
 
         _rb_question_6 = findViewById(R.id.rb_question_6);
+        InitializeSmileRating(_rb_question_6);
         _rb_question_6.setTag(6);
 
         _rb_question_7 = findViewById(R.id.rb_question_7);
+        InitializeSmileRating(_rb_question_7);
         _rb_question_7.setTag(7);
 
         _rb_question_8 = findViewById(R.id.rb_question_8);
+        InitializeSmileRating(_rb_question_8);
         _rb_question_8.setTag(8);
 
         _rb_question_9 = findViewById(R.id.rb_question_9);
+        InitializeSmileRating(_rb_question_9);
         _rb_question_9.setTag(9);
 
         _rb_question_10 = findViewById(R.id.rb_question_10);
+        InitializeSmileRating(_rb_question_10);
         _rb_question_10.setTag(10);
 
         _et_question_11 = findViewById(R.id.et_question_11);
@@ -105,7 +120,8 @@ public class RatingCourseAct extends AppCompatActivity {
         _et_question_15 = findViewById(R.id.et_question_15);
         _et_question_15.setTag(15);
 
-        Dictionary<Integer, RatingBar> rb_questions = new Hashtable<>();
+
+        Dictionary<Integer, SmileRating> rb_questions = new Hashtable<>();
         rb_questions.put(0, _rb_question_1);
         rb_questions.put(1, _rb_question_2);
         rb_questions.put(2, _rb_question_3);
@@ -138,14 +154,23 @@ public class RatingCourseAct extends AppCompatActivity {
         });
     }
 
-    private boolean sendRatings(Dictionary<Integer, RatingBar> rb_questions, Dictionary<Integer, EditText> et_questions) {
+    private void InitializeSmileRating(SmileRating smileRating) {
+        smileRating.setSelectedSmile(BaseRating.OKAY);
+        smileRating.setNameForSmile(BaseRating.TERRIBLE, getString(R.string.smile_lv1));
+        smileRating.setNameForSmile(BaseRating.BAD, getString(R.string.smile_lv2));
+        smileRating.setNameForSmile(BaseRating.OKAY, getString(R.string.smile_lv3));
+        smileRating.setNameForSmile(BaseRating.GOOD, getString(R.string.smile_lv4));
+        smileRating.setNameForSmile(BaseRating.GREAT, getString(R.string.smile_lv5));
+    }
+
+    private boolean sendRatings(Dictionary<Integer, SmileRating> rb_questions, Dictionary<Integer, EditText> et_questions) {
         try {
             for (int i = 0; i < 10; i++) {
 
                 _callWebAPI.createDetailEvaluationCourse(
                         _id_course,
                         Integer.parseInt(rb_questions.get(i).getTag().toString()),
-                        rb_questions.get(i).getRating(),
+                        Float.parseFloat(String.valueOf(rb_questions.get(i).getRating())),
                         true,
                         "",
                         Constants.ACCOUNT_LOGIN.getEmail()
